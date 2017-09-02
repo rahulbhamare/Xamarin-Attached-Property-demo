a917586bacb0130c532cb4292035c0eb05166464
Rahul Darekar <rahul.darekar@barclays.com>
2017-09-02 16:02:38 +0530

    Xamarin : How to use Attached Property

---
+++ AttachedPropertyDemo/AttachedPropertyDemo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AttachedPropertyDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if(MainPageViewModel.GetIsAttached(WelcomeText))
+                WelcomeText.Text = "Welcome to Xamarin Forms! This is Attached Property demo.";
+        }
+    }
+}
---
+++ AttachedPropertyDemo/AttachedPropertyDemo/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AttachedPropertyDemo
+{
+    public class MainPageViewModel
+    {
+        //Created a Attached Property with IsAttached name
+        public static readonly BindableProperty IsAttachedProperty =
+  BindableProperty.CreateAttached("IsAttached", typeof(bool), typeof(MainPage),false);
+
+        //Get IsAttached property value
+        public static bool GetIsAttached(BindableObject view)
+        {
+            return (bool)view.GetValue(IsAttachedProperty);
+        }
+
+        //Set IsAttached property value
+        public static void SetIsAttached(BindableObject view, bool value)
+        {
+            view.SetValue(IsAttachedProperty, value);
+        }
+    }
+}
